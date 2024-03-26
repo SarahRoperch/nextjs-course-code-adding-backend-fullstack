e7ef2d72c3aa10fa6593dcfe6051e3d72e0257e8
Sarah Roperch <sarah.roperch@nexelec.fr>
2024-03-26 16:46:24 +0100

    first commit finished

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+
+function HomePage() {
+  const [feedbackItems, setFeedbackItems] = useState();
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: { "Content-type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  function loadFeedbackHandler() {
+    fetch("/api/feedback")
+      .then((response) => response.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  }
+
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef}></input>
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback Address</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems ? (
+          feedbackItems.map((item) => <li key={item.id}>{item.text}</li>)
+        ) : (
+          <li>No feedback items available</li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
